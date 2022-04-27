316cf9c72140d841fbf949bb4861e1250ee54d53
HenryClones <henryjones@programmer.net>
2022-04-27 10:42:52 -0400

    Begin work on topbar

---
+++ topbar.php
@@ -0,0 +1,27 @@
+<?php
+include("api/v1/guard.php");
+?>
+<div class="top-bar">
+    <div class="top-bar-left">
+        <ul class="dropdown menu" data-dropdown-menu>
+            <li class="menu-text">ADS.com</li>
+            <li>
+                <a href="#">Offers</a>
+                <ul class="menu vertical">
+                    <li><a href="#">Renters</a></li>
+                    <li><a href="#">Clients</a></li>
+                    <li><a href="#">Products/Services</a></li>
+                </ul>
+            </li>
+            <li><a href="#"></a></li>
+            <li><a href="#"></a></li>
+        </ul>
+    </div>
+    <div class="top-bar-right">
+        <ul class="menu">
+            <!-- TODO: Add login option here with modal replaced with account dropdown -->
+            <li><input type="search" placeholder="Search"></li>
+            <li><button type="button" class="button">Search</button></li>
+        </ul>
+    </div>
+</div>
