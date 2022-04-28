96df31e50d46e0f11cfde247208b487c56c521ee
isaacmallen <allenim@appstate.edu>
2022-04-28 15:53:27 -0400

    safety push

--- topbar.php
+++ topbar.php
@@ -24,9 +24,82 @@ include("api/v1/guard.php");
     <div class="top-bar-right">
         <ul class="menu">
             <!-- TODO: Add login option here with modal replaced with account dropdown -->
-            <li><input type="search" placeholder="Search"></li>
+	    <li>
+
+	    <div id="login-icon">
+		<?php 
+		    echo '
+		    <img src="'.getenv(path).'icon/icon-login.png" height="10">
+		    ';
+		?>
+	    </div>
+	    </li>
+	    <li><input type="search" placeholder="Search"></li>
             <li><button type="button" class="button">Search</button></li>
         </ul>
     </div>
 </div>
+
+	    <div id="login-popup">
+	            <form class="login-form" action="" id="login-form"
+	                method="post" enctype="multipart/form-data">
+	                <h1>Login</h1>
+	                <div>
+	                    <div>
+	                        <label>Name: </label><span id="userName-info"
+	                            class="info"></span>
+	                    </div>
+	                    <div>
+	                        <input type="text" id="userName" name="userName"
+	                            class="inputBox" />
+	                    </div>
+	                </div>
+	                <div>
+	                    <div>
+	                        <label>Email: </label><span id="userEmail-info"
+	                            class="info"></span>
+	                    </div>
+	                    <div>
+	                        <input type="text" id="userEmail" name="userEmail"
+	                            class="inputBox" />
+	                    </div>
+	    	    </div>
+	    	    <div>
+	                    <input type="submit" id="send" name="send" value="Send" />
+	                </div>
+	            </form>
+	    </div>
+<script>
+$(document).ready(function () {
+    $("#login-icon").click(function () {
+	$("#login-popup").show();
+    });
+    //Login Form validation on click event
+	$("#login-form").on("submit", function () {
+	    var valid = true;
+	    $(".info").html("");
+	    $("inputBox").removeClass("input-error");
+
+	    var userName = $("#userName").val();
+	    var userEmail = $("#userEmail").val();
+
+	    if (userName == "") {
+		$("#userName-info").html("required.");
+		$("#userName").addClass("input-error");
+	    }
+	    if (userEmail == "") {
+		$("#userEmail-info").html("required.");
+		$("#userEmail").addClass("input-error");
+		valid = false;
+	    }
+	    if (!userEmail.match(/^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/))
+	    {
+		$("#userEmail-info").html("invalid.");
+		$("#userEmail").addClass("input-error");
+		valid = false;
+	    }
+	    return valid;
+	});
+});
+</script>
 </nav>
