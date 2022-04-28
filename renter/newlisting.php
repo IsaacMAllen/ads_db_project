35665df73e4e40ca907a7e830f879e8d379c22b2
isaacmallen <allenim@appstate.edu>
2022-04-28 11:01:56 -0400

    added blob uploading

---
+++ renter/newlisting.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Add a new listing</title>
+</head>
+<body>
+    <?php
+	$host = 'localhost';
+	$user = 'allenim';
+	$pass = 'Appstate123';
+	$dbname = 'CS_2022_Spring_3430_101_t1';
+	try {
+	    $connection = new PDO("mysql:host=$host;dbname=$dbname", $user, $pass);
+	    $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	    echo("Connected to $dbname at $host successfully.\n");
+	} catch(PDOException $pe) {
+	    die("Could not connect to the database $dbname :" . $pe->getMessage());
+	}
+	if($_SERVER['REQUEST_METHOD'] == 'POST'){
+	    $productId = uniqid();
+	    echo($productId . "\n");
+	    $cost = 12;
+	    $transactionId = null;
+	    $renterId = null;
+	    $picture = file_get_contents($_FILES['listImg']['tmp_name']);
+	    $query = "SELECT count(*) as allcount FROM project_product
+		      WHERE picture='".$picture."'";
+	    $stmt = $connection->prepare($query);
+	    $row = 
+	    $stmt = $connection->prepare('INSERT INTO project_product(productId, cost, transactionId, renterId, picture)
+				    VALUES(?, ?, ?, ?, ?)');
+	    $stmt -> bindParam(1,$productId);
+	    $stmt -> bindParam(2,$cost);
+	    $stmt -> bindParam(3,$transactionId);
+	    $stmt -> bindParam(4,$renterId);
+	    $stmt -> bindParam(5,$picture);
+	    try {
+	    $passed = $stmt -> execute();
+	    if ($passed) {
+		echo "passed";
+	    } else {
+		print_r($dbh->errorInfo());
+	    }
+	    } catch (PDOException $e) {
+		die($e -> getMessage());
+	    }
+	    header('HTTP/1.1 301 Moved Permanently');
+	    header('Location: success.php');
+	    die();
+	}    
+    ?>
+    <form enctype="multipart/form-data" method="POST" name="frm_user_file">
+	<input type="file" name="listImg"/> 
+	<input type="submit" value="Upload"/>
+    </form>
+</body>
+</html>
---
+++ renter/success.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+	<title>Listed Successfully</title>
+    </head>
+    <body>
+
+	<h1>Thank you for listing your product with us.</h1>
+
+	<p>Your product has been listed successfully</p>
+
+    </body>
+</html>
