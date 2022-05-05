3c48d8e705287bffc6ab24c91c263dab3d3cec64
HenryClones <henryjones@programmer.net>
2022-05-05 08:42:01 -0400

    Add form and form processing to submit product

--- account/index.php
+++ account/index.php
@@ -1,10 +1,9 @@
 <?php
-ini_set("display_errors", 1);
 include_once("../getvars.php");
-if ($_SERVER["QUERY_STRING"]["newid"]) {
+$newid = $params["newid"];
+if ($newid) {
     header("Location: ../?id=$newid");
 }
-session_start();
 if ($slist or $info or !$_SESSION["user"]) {
     header("Location: ../?" . $_SERVER['QUERY_STRING']);
 }
@@ -13,15 +12,21 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     include_once("../api/v1/access.php");
     $cost = $_POST["cost"];
     $name = $_POST["name"];
-    $picture = $_POST["picture"];
+    $picture = $_FILES["picture"];
+    if ($picture["error"] > 0) {
+        echo "Error processing image: " . $picture["error"] . "<br>";
+        die();
+    } else {
+        $picture = file_get_contents($picture["tmp_name"]);
+    }
     $tflag = $_POST["type"];
     $renterId = $_SESSION["user"];
     $productId = strtr(base64_encode(uniqid()), '+/=', '._-');
     // Send the query
     include_once("../api/v1/access.php");
     $stmt = $pdo->prepare("INSERT INTO project_product VALUES (?, ?, ?, NULL, ?, ?, ?, CURRENT_TIMESTAMP)");
-    $stmt->execute($productId, $name, $cost, $renterId, $picture, $tflag);
-    header("Location: ../?newid=$productId");
+    $stmt->execute(array($productId, $name, $cost, $renterId, $picture, $tflag));
+    header("Location: ./?newid=$productId");
 }
 ?>
 <!DOCTYPE html>
@@ -31,16 +36,16 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
 </head>
 <body>
     <?php include_once("../topbar.php"); ?>
-    <form method="post" enctype="multipart/form-data" class="search-form"
+    <form method="post" enctype="multipart/form-data" class="submit-product"
         action="<?php echo "https://${sname}$directoryname/" ?>">
         <input type="textbox" placeholder="Product name" name="name" id="name"/>
         <input type="textbox" placeholder="Cost" id="cost" name="cost"/>
         <input type="file" accept="image/jpg" name="picture" id="picture"/>
-        <select id="tflag" name="tflag" size="2">
+        <select id="type" name="type" size="2">
             <option value="I">Item</option>
             <option value="S">Service</option>
         </select>
-        <input type="submit" id="submit" name="submit"/>
+        <input type="Submit" id="submit" name="submit"/>
     </form>
     <script>
     $(document).foundation();
